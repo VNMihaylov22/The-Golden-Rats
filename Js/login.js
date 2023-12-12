179bccc75da27f2bc13b3c85f82c7f9bdc3091b2
VBKanev22 <VBKanev22@codingburgas.bg>
2023-12-12 22:33:21 +0200

    add comments and finish project

--- Js/login.js
+++ Js/login.js
@@ -1,21 +1,30 @@
+// Selecting necessary elements from the DOM
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
 
-registerLink.addEventListener('click', ()=> {
+// Adding event listener for the Register link
+registerLink.addEventListener('click', () => {
+    // Adding the 'active' class to the wrapper on Register link click
     wrapper.classList.add('active');
 });
 
-loginLink.addEventListener('click', ()=> {
+// Adding event listener for the Login link
+loginLink.addEventListener('click', () => {
+    // Removing the 'active' class from the wrapper on Login link click
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=> {
+// Adding event listener for the Login button
+btnPopup.addEventListener('click', () => {
+    // Adding the 'active-popup' class to the wrapper on Login button click
     wrapper.classList.add('active-popup');
 });
 
-iconClose.addEventListener('click', ()=> {
+// Adding event listener for the close icon
+iconClose.addEventListener('click', () => {
+    // Removing the 'active-popup' class from the wrapper on close icon click
     wrapper.classList.remove('active-popup');
 });
--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,4 @@
+// Function to toggle dropdown visibility
 function toggleDropdown() {  
     var dropdownContent = document.getElementById("myDropdown");
     if (dropdownContent.style.display === "block") {
@@ -7,24 +8,30 @@ function toggleDropdown() {
     }
 }
 
-
+// Array to store cart products
 var cartProducts = [];
 
+// Function to add items to the cart
 function addToCart(button) {
     var product = button.parentNode;
     var productName = product.dataset.name;
     var productPrice = parseFloat(product.dataset.price);
 
+    // Create a new cart item
     var cartItem = document.createElement('li');
     cartItem.innerHTML = `${productName} - $${productPrice.toFixed(2)} <button onclick="removeFromCart('${productName}')">Remove</button>`;
 
+    // Append the cart item to the cart list
     document.getElementById('cart-items').appendChild(cartItem);
 
+    // Add product to the cartProducts array
     cartProducts.push({ name: productName, price: productPrice });
 
+    // Update total price
     updateTotal(productPrice);
 }
 
+// Function to update the total price
 function updateTotal(price) {
     var totalElement = document.getElementById('total');
     var currentTotal = parseFloat(totalElement.textContent);
@@ -34,6 +41,7 @@ function updateTotal(price) {
     totalElement.textContent = newTotal.toFixed(2);
 }
 
+// Function to remove items from the cart
 function removeFromCart(productName) {
     var index = cartProducts.findIndex(product => product.name === productName);
     if (index !== -1) {
@@ -52,50 +60,7 @@ function removeFromCart(productName) {
     }
 }
 
-var cartProducts = [];
-
-function addToCart(button) {
-    var product = button.parentNode;
-    var productName = product.dataset.name;
-    var productPrice = parseFloat(product.dataset.price);
-
-    var cartItem = document.createElement('li');
-    cartItem.innerHTML = `${productName} - $${productPrice.toFixed(2)} <button onclick="removeFromCart('${productName}')">Remove</button>`;
-
-    document.getElementById('cart-items').appendChild(cartItem);
-
-    cartProducts.push({ name: productName, price: productPrice });
-
-    updateTotal(productPrice);
-}
-
-function updateTotal(price) {
-    var totalElement = document.getElementById('total');
-    var currentTotal = parseFloat(totalElement.textContent);
-
-    var newTotal = currentTotal + price;
-
-    totalElement.textContent = newTotal.toFixed(2);
-}
-
-function removeFromCart(productName) {
-    var index = cartProducts.findIndex(product => product.name === productName);
-    if (index !== -1) {
-        var removedPrice = cartProducts[index].price;
-        cartProducts.splice(index, 1);
-        updateTotal(-removedPrice);
-    }
-
-    var cartItems = document.getElementById('cart-items');
-    var items = cartItems.getElementsByTagName('li');
-    for (var i = 0; i < items.length; i++) {
-        if (items[i].textContent.includes(productName)) {
-            cartItems.removeChild(items[i]);
-            break;
-        }
-    }
-}
-
+// Function to clear the cart
 function clearCart() {
     cartProducts = [];
     document.getElementById('cart-items').innerHTML = '';
